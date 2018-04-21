1f3cbaf9268b6fb9676bf766049cd4fefb5667cf
regisf <regisfloret@protonmail.com>
2018-04-21 11:14:32 +0200

    Change owner

--- src/file.c
+++ src/file.c
@@ -1,6 +1,6 @@
 /*
  * Geoclustering micro service 
- * (c) Régis FLORET 2018
+ * (c) Prince Cuberdon 2018
  * 
  * Redistribution and use in source and binary forms, with or without 
  * modification, are permitted provided that the following conditions are met:
